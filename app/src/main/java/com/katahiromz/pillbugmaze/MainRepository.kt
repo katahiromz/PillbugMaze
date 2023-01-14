5511567a2a14565f0cbf976c4923b88115ba3fb0
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2023-01-14 10:01:22 +0900

    first commit

---
+++ app/src/main/java/com/katahiromz/pillbugmaze/MainRepository.kt
@@ -0,0 +1,18 @@
+package com.katahiromz.krakra_ja_jp
+
+import android.content.Context
+import android.content.SharedPreferences
+import org.json.JSONArray
+
+class MainRepository {
+
+    companion object {
+
+        private const val MainPrefFileKey = "MAIN_PREF_FILE"
+        private const val MessageListKey = "MESSAGE_LIST"
+
+        private fun getPrefs(context: Context): SharedPreferences {
+            return context.getSharedPreferences(MainPrefFileKey, Context.MODE_PRIVATE)
+        }
+   }
+}
