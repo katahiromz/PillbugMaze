561ee2d0f608d14041a90ec40311a054fd994844
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2023-01-19 13:32:11 +0900

    myList

--- app/src/main/java/com/katahiromz/pillbugmaze/MainRepository.kt
+++ app/src/main/java/com/katahiromz/pillbugmaze/MainRepository.kt
@@ -3,6 +3,7 @@ package com.katahiromz.pillbugmaze
 import android.content.Context
 import android.content.SharedPreferences
 import org.json.JSONArray
+import java.util.Collections.emptyList
 
 class MainRepository {
 
@@ -16,6 +17,7 @@ class MainRepository {
         // TODO: 設定項目を追加
         var myFloat: Float = 0.0f
         var myInt: Int = 0
+        var myList: MutableList<String> = mutableListOf<String>()
 
         // 共有設定を取得する。
         private fun getPreferences(context: Context): SharedPreferences {
@@ -26,20 +28,28 @@ class MainRepository {
         fun load(context: Context) {
             val prefs = getPreferences(context)
             // TODO: データバージョンのチェック。
-            val data_version:Int = prefs.getInt("data_version", 0)
+            val dataVersion: Int = prefs.getInt("dataVersion", 0)
             // TODO: 設定項目を読み込む
             myFloat = prefs.getFloat("myFloat", 0.0f)
             myInt = prefs.getInt("myInt", 0)
+            val myListStr: String? = prefs.getString("myList", "")
+            if (myListStr != null) {
+                myList = myListStr.split("<>").toMutableList()
+            } else {
+                myList.clear()
+            }
         }
 
         // 設定を保存する。
         fun save(context: Context) {
             // TODO: 設定項目を書き込む
+            val myListStr = myList.joinToString("<>")
             getPreferences(context)
                     .edit()
-                    .putInt("data_version", DATA_VERSION)
+                    .putInt("dataVersion", DATA_VERSION)
                     .putInt("myInt", myInt)
                     .putFloat("myFloat", myFloat)
+                    .putString("myList", myListStr)
                     .apply()
         }
    }
